a313eb66bbdcc6fae29b4c1c6771aa622a7aa3cb
KJ002 <jamesbutcher167@gmail.com>
2021-05-23 14:24:08 +0100

    Altered setup.py to work with pyproject.toml

--- setup.py
+++ setup.py
@@ -2,12 +2,10 @@ from glob import glob
 from setuptools import setup
 from pybind11.setup_helpers import Pybind11Extension
 
-
 ext_modules = [
     Pybind11Extension(
         "Parser",
-        sorted(glob("src/*.cpp")),
-        include_dirs=['include']
+        sorted(glob('*.cpp')),
     )
 ]
 
